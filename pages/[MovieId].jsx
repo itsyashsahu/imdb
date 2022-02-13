3daf5670137988e83ffbe09c6873edbff8005ff9
Yash Sahu <itsyashsahu@gmail.com>
2022-02-13 22:42:03 +0530

    adding svg loaders

--- pages/[MovieId].jsx
+++ pages/[MovieId].jsx
@@ -2,13 +2,16 @@ import React, { useEffect,useState } from "react";
 import { useRouter } from "next/router";
 import Header from "../comp/Header";
 import Head from "next/head";
-
+import loadingsvg from '../comp/loading.svg'
+import Image from "next/image";
+// import { set } from "mongoose";
 
 export default function Movie({ApiKey}) {
   const router = useRouter();
   const [movie,setMovie ] = useState({});
   const [ Upvotes , setUpvotes ] = useState(0);
   const [ Downvotes , setDownvotes ] = useState(0);
+  const [ serchLoader , setSearchLoader ] = useState(true)
   
   const [ fromSearch , setFromSeatch ] = useState(false);
   const movieId = router.query.MovieId;
@@ -18,6 +21,7 @@ export default function Movie({ApiKey}) {
       .then(async function (res) {
         const response = await res.json();
         // setResults(response.Search);
+        setSearchLoader(false);
         setMovie(response);
         setFromSeatch(true);
         // setUpvotes( +response?.Upvotes)
@@ -44,6 +48,7 @@ export default function Movie({ApiKey}) {
         // implement the logic if response has length 0 
         if( response.length > 0 ){
           setMovie(response[0]);
+          setSearchLoader(false);
           setUpvotes( +response[0]?.Upvotes)
           setDownvotes( +response[0]?.Downvotes)
         }else{
@@ -140,86 +145,93 @@ export default function Movie({ApiKey}) {
         <title>Movie : {movie?.Title}</title>
       </Head>
       <Header />
-      <section className="text-gray-600 body-font overflow-hidden h-4/6">
-        <div className="container px-5 py-10 mx-auto">
-          <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center">
-            <div className="w-96 h-[30rem] max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <img
-                className="h-full w-full"
-                src={movie?.Poster}
-                // src="https://m.media-amazon.com/images/M/MV5BYmRmYzA4NDItZmI3NS00NWIyLWIzZWEtNGIzZjZlYmY5MzE2XkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg"
-              />
-            </div>
-            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                {movie?.Title}
-              </h1>
-              <div className="flex mb-4 mr-4">
-                <span className="flex items-center mr-4">
-                  <span className="text-gray-600">Genre : {movie?.Genre}</span>
-                </span>
-                <span className="flex ml-3 pl-3 py-0 border-l-2 border-gray-200 space-x-2s">
-                  <span className="text-gray-600 ml-3">Year : {movie?.Year}</span>
-                </span>
+      {
+        (serchLoader)?
+        <div className="bg-gray-800 h-screen flex justify-center items-center" >
+          <Image src={loadingsvg} />
+        </div>
+        :
+        <section className="text-gray-600 body-font overflow-hidden h-4/6">
+          <div className="container px-5 py-10 mx-auto">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center">
+              <div className="w-96 h-[30rem] max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <img
+                  className="h-full w-full"
+                  src={movie?.Poster}
+                  // src="https://m.media-amazon.com/images/M/MV5BYmRmYzA4NDItZmI3NS00NWIyLWIzZWEtNGIzZjZlYmY5MzE2XkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg"
+                />
               </div>
-              <p className="leading-relaxed pb-5">
-                <span className="text-gray-600">Released On : {movie?isoToDate(movie.Released):null}</span>
-                <br />
-                <span className="text-gray-600">Runtime : {movie?.Runtime}</span>
-                <br />
-                <span className="text-gray-600">
-                  Directors : {movie?.Director}
-                </span>
-                <br />
-                <span className="text-gray-600">Actors : {movie?.Actors}</span>
-                <br />
-                <span className="text-gray-600">Writers : {movie?.Writer}</span>
-                <br />
-                <span className="text-gray-600">
-                  Languages : {movie?.Language}
-                </span>
-                <br />
-                <br />
-                {/* plot */}
-                {movie?.Plot}
-              </p>
-              <div className="flex justify-between pt-2 border-t-2 border-gray-100 ">
-                <span className="flex items-center title-font font-medium text-2xl text-gray-900">
-                  Upvotes : {Upvotes} &nbsp;
-                  <button
-                    onClick={upvote}
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                  {movie?.Title}
+                </h1>
+                <div className="flex mb-4 mr-4">
+                  <span className="flex items-center mr-4">
+                    <span className="text-gray-600">Genre : {movie?.Genre}</span>
+                  </span>
+                  <span className="flex ml-3 pl-3 py-0 border-l-2 border-gray-200 space-x-2s">
+                    <span className="text-gray-600 ml-3">Year : {movie?.Year}</span>
+                  </span>
+                </div>
+                <p className="leading-relaxed pb-5">
+                  <span className="text-gray-600">Released On : {movie?isoToDate(movie.Released):null}</span>
+                  <br />
+                  <span className="text-gray-600">Runtime : {movie?.Runtime}</span>
+                  <br />
+                  <span className="text-gray-600">
+                    Directors : {movie?.Director}
+                  </span>
+                  <br />
+                  <span className="text-gray-600">Actors : {movie?.Actors}</span>
+                  <br />
+                  <span className="text-gray-600">Writers : {movie?.Writer}</span>
+                  <br />
+                  <span className="text-gray-600">
+                    Languages : {movie?.Language}
+                  </span>
+                  <br />
+                  <br />
+                  {/* plot */}
+                  {movie?.Plot}
+                </p>
+                <div className="flex justify-between pt-2 border-t-2 border-gray-100 ">
+                  <span className="flex items-center title-font font-medium text-2xl text-gray-900">
+                    Upvotes : {Upvotes} &nbsp;
+                    <button
+                      onClick={upvote}
+                      >
+                      <svg
+                        width="24px"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10zM15 12h-1v8h-4v-8H6.081L12 4.601 17.919 12H15z" />
+                      </svg>
+                    </button>
+                  </span>
+                  <span className="flex items-center title-font font-medium text-2xl text-gray-900">
+                    Downvotes : {Downvotes} &nbsp;
+                    <button
+                      onClick={downvote}
                     >
-                    <svg
-                      width="24px"
-                      height="24px"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10zM15 12h-1v8h-4v-8H6.081L12 4.601 17.919 12H15z" />
-                    </svg>
-                  </button>
-                </span>
-                <span className="flex items-center title-font font-medium text-2xl text-gray-900">
-                  Downvotes : {Downvotes} &nbsp;
-                  <button
-                    onClick={downvote}
-                  >
-                    <svg
-                      width="24px"
-                      height="24px"
-                      viewBox="0 0 24 24"
-                      className=""
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059zM12 19.399 6.081 12H10V4h4v8h3.919L12 19.399z" />
-                    </svg>
-                  </button>
-                </span>
+                      <svg
+                        width="24px"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        className=""
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059zM12 19.399 6.081 12H10V4h4v8h3.919L12 19.399z" />
+                      </svg>
+                    </button>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      }
     </>
   );
 }
--- pages/search/[searchTerm].js
+++ pages/search/[searchTerm].js
@@ -3,11 +3,16 @@ import { useRouter } from "next/router";
 import Header from "../../comp/Header";
 import Card2 from "../../comp/Card2";
 import Head from "next/head";
+import loadingsvg from '../../comp/loading.svg'
+import Image from "next/image";
+
 
 export default function SearchHandler({ ApiKey }) {
   const router = useRouter();
   const searchTerm = router.query.searchTerm;
   const [ Results , setResults ] = useState([]);
+  const [ serchLoader , setSearchLoader ] = useState(true)
+
   // http://www.omdbapi.com/?s=Batman&page=2&apikey=ce4796de
   // const ApiKey = process.env.API_KEY;
   // console.log(ApiKey)
@@ -16,6 +21,7 @@ export default function SearchHandler({ ApiKey }) {
       .then(async function (res) {
         const response = await res.json();
         setResults(response.Search);
+        setSearchLoader(false)
         console.log(response);
       })
       .catch(function (res) {
@@ -33,30 +39,40 @@ export default function SearchHandler({ ApiKey }) {
         <title>{`Search Results - ${searchTerm} `}</title>
       </Head>
       <Header />
-      <div className="flex justify-between mt-8">
-        <h1 
-        onClick={()=>{ console.log("healkjdsh");  router.push("/search/abc")}}
-        className="flex lg:px-8 md:px-10 px-5 lg:mx-16 md:mx-10 mx-4 font-bold text-3xl text-gray-800">
-          Search Results - {searchTerm}
-        </h1>
-      </div>
-
-      <div className="container my-5 mx-auto px-4 md:px-12">
-        <div className="flex flex-wrap -mx-1 lg:-mx-4">
-          {Results?.map((movie) => {
-            // console.log("all",movie.imdbID)
-            return (
-                <Card2
-                  key={movie.imdbID}
-                  poster={movie.Poster}
-                  title={movie.Title}
-                  genre={movie.Type}
-                  imdbId={movie.imdbID}
-                />
-            );
-          })}
+      {
+        (serchLoader)?
+        <div className="bg-gray-800 h-screen flex justify-center items-center" >
+          <Image src={loadingsvg} />
+          Hello World
         </div>
-      </div>
+        :
+        <>
+        <div className="flex justify-between mt-8">
+          <h1 
+          onClick={()=>{ console.log("healkjdsh");  router.push("/search/abc")}}
+          className="flex lg:px-8 md:px-10 px-5 lg:mx-16 md:mx-10 mx-4 font-bold text-3xl text-gray-800">
+            Search Results - {searchTerm}
+          </h1>
+        </div>
+
+        <div className="container my-5 mx-auto px-4 md:px-12">
+          <div className="flex flex-wrap -mx-1 lg:-mx-4">
+            {Results?.map((movie) => {
+              // console.log("all",movie.imdbID)
+              return (
+                  <Card2
+                    key={movie.imdbID}
+                    poster={movie.Poster}
+                    title={movie.Title}
+                    genre={movie.Type}
+                    imdbId={movie.imdbID}
+                  />
+              );
+            })}
+          </div>
+        </div>
+        </>
+      }
     </>
   );
 }
